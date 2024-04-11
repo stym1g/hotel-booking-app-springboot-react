d03ff7401cf8e590f991125e8fa5d809e070af0a
Satyam Gupta <satyam@Satyams-MacBook-Air.local>
2024-04-11 23:46:29 +0530

    initialized maven project

---
+++ src/main/java/com/example/hotelbookingappspringbootreact/HotelBookingAppSpringbootReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotelbookingappspringbootreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingAppSpringbootReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBookingAppSpringbootReactApplication.class, args);
+    }
+
+}
