344b03aaf91a24216bfd5a27dcd30efcb65f4904
satyam <satyamg650@gmail.com>
2024-05-17 01:11:50 +0530

    added bootstrap node-module

--- client/hotel-booking-app/src/App.jsx
+++ client/hotel-booking-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import './App.css'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
 import AddRoom from './components/rooms/AddRoom'
 
 function App() {
